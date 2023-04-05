2f981f1ccf49b65a9fc83df80c9575f4ef37211c
gabsgregorio <gabsufrrj@gmail.com>
2023-04-05 17:48:42 -0300

    feat: add components to angular archives

--- minsait-frontend/src/app/app.module.ts
+++ minsait-frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ClientesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
