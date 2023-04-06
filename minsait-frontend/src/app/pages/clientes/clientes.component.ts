dee4e5b44b09d8c26e185ea4a7436408ec8c5d44
gabsgregorio <gabsufrrj@gmail.com>
2023-04-06 16:25:42 -0300

    feat: add delete method

--- minsait-frontend/src/app/pages/clientes/clientes.component.ts
+++ minsait-frontend/src/app/pages/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, Directive, Injectable, Input, NgModule, Output, Pipe } from '@angular/core';
 import { ICliente } from 'src/app/interfaces/ICliente';
 import { ClientesService } from 'src/app/services/clientes.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -12,8 +13,18 @@ export class ClientesComponent {
   constructor(private clientesService: ClientesService) {}
 
   ngOnInit() {
+
     this.clientesService.findAllCustomers().subscribe((result: ICliente[])  => {
       this.clientes = result;
     });
+  }
+
+  deleteCustomer(cpf: number) {
+    console.log(cpf);
+    this.clientesService.deleteCustomer(cpf).subscribe(_result => {
+      this.ngOnInit();
+      Swal.fire('Sucesso!', 'Cliente deletado com sucesso.', 'success');
+    });
+
   }
 }
